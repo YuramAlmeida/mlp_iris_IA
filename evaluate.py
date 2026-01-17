9e3f98b02365371e9a234c7acb4f75a3aa627762
YuramAlmeida <151592864+YuramAlmeida@users.noreply.github.com>
2026-01-17 19:37:17 -0300

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1,15 +1,18 @@
 from train import train
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 
 def evaluate():
     model, X_test, y_test = train()
 
     y_pred = model.predict(X_test)
 
+    print("\nAcurácia:")
+    print(accuracy_score(y_test, y_pred))
+
     print("\nRelatório de Classificação:")
     print(classification_report(y_test, y_pred))
 
-    print("Matriz de confusão:")
+    print("Matriz de Confusão:")
     print(confusion_matrix(y_test, y_pred))
 
 if __name__ == "__main__":
